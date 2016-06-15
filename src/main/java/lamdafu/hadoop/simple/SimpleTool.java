03484cf5311f4f80eabaf49f79b46dd57fa8e577
Matt Pouttu-Clarke <voidpointer101@gmail.com>
2016-06-15 16:59:07 -0700

    Added main

--- src/main/java/lamdafu/hadoop/simple/SimpleTool.java
+++ src/main/java/lamdafu/hadoop/simple/SimpleTool.java
@@ -6,6 +6,7 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
 /**
  * Doesn't do much but thats the point :)
@@ -22,5 +23,11 @@ public class SimpleTool extends Configured implements Tool {
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		return job.waitForCompletion(true) ? 0 : -1;
 	}
+	
+    public static void main(String... args) throws Exception {
+    	SimpleTool tool = new SimpleTool();
+        int res = ToolRunner.run(tool.getConf(), tool, args);
+        System.exit(res);
+    }
 
 }
